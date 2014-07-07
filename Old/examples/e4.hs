496f568d3a7afeaa5c12f9364b068a4b328bf5e2
kaizhang <zk65900931@gmail.com>
2014-07-07 10:10:47 -0700

    delete old package

--- Old/examples/e4.hs
+++
@@ -1,8 +0,0 @@
-import Graphics.Rendering.HPlot
-import Data.DataSets (anscombe)
-import Data.Vector
-
-main = do
-    (labs, xs) <- anscombe
-
-    heatmap' (fmap toList xs) (with & labRow .~ labs) "e4.png"
